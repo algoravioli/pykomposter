437ae0a5299a9016a72db38ddf5cc5d73de43e59
christopherclarkeSUTD <christopher_clarke@mymail.sutd.edu.sg>
2022-01-24 11:02:45 +0800

    commit

---
+++ lib.py
@@ -0,0 +1,22 @@
+import fractions
+import sys
+import time
+
+import music21
+import numpy as np
+import pandas as pd
+import tqdm
+
+
+class pykomposter:
+    def __init__(self):
+        super(pykomposter, self).__init__()
+        self.outlook = dict()
+
+    def train(self):
+        print("HI")
+
+
+myKomposter = pykomposter()
+
+myKomposter.func()
